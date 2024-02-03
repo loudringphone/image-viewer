ab2eb2e4836de44759fb64cde1786bfbd251dbc0
loudringphone <loudringphone@gmail.com>
2024-02-03 21:06:21 +1100

    upload image controller done

--- app/javascript/controllers/upload_controller.js
+++ app/javascript/controllers/upload_controller.js
@@ -2,20 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="upload"
 export default class extends Controller {
-  static targets = ["title", "attachment", "preview"];
+  static targets = ['title', 'attachment', 'preview', 'submit'];
 
   connect() {
-    const titleElement = this.titleTarget;
-    console.log(titleElement)
   }
   previewImage(){
-    console.log(this.attachmentTarget.files)
     const preview = this.previewTarget
-    const file = this.attachmentTarget.files[0];
-    const reader = new FileReader();
-    reader.onload = function(e) {
-      preview.src = e.target.result;
+    const filesLength = this.attachmentTarget.files.length
+    if (filesLength > 0) {
+      const file = this.attachmentTarget.files[0];
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        preview.src = e.target.result;
+      }
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = '';
     }
-    reader.readAsDataURL(file);
+    this.handleInput()
+  }
+  handleInput() {
+    const title = this.titleTarget
+    const filesLength = this.attachmentTarget.files.length
+    if (title.value.trim() !== '' && filesLength > 0) {
+      this.enableSubmit()
+    } else {
+      this.disableSubmit()
+    }
+  }
+  disableSubmit() {
+    this.submitTarget.disabled = true;
+    this.submitTarget.style.opacity = '0.5'
+    this.submitTarget.style.pointerEvents = 'default'
+
+  }
+  enableSubmit() {
+    this.submitTarget.disabled = false;
+    this.submitTarget.style.opacity = '1'
+    this.submitTarget.style.pointerEvents = 'auto'
   }
 }
