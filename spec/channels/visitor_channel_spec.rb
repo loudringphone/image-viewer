566f30360213bfbdc31c0307c88a9025faf561c1
loudringphone <loudringphone@gmail.com>
2024-02-04 21:21:03 +1100

    visitor channel spec

--- spec/channels/visitor_channel_spec.rb
+++ spec/channels/visitor_channel_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe VisitorChannel, type: :channel do
   let(:image) { create(:image) }
   let(:user_count) { 0 }
 
-  before do
-    stub_connection
-    allow(REDIS).to receive(:get).with("user_count_#{image.id}").and_return({ user_count: user_count }.to_json)
-  end
-
   it "subscribes to a stream when room id is provided" do
+    expect(REDIS).to receive(:get).with("user_count_#{image.id}").and_return({ user_count: }.to_json)
+
+    stub_connection
+    allow(REDIS).to receive(:get).and_call_original
+
     subscribe(id: image.id)
     expect(subscription).to be_confirmed
     expect(subscription).to have_stream_from("visitor_channel_#{image.id}")
+    expect(REDIS.get("user_count_1")).to eq "{\"user_count\":#{ user_count + 1 }}"
   end
 end
