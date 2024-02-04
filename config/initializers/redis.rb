d1feeb0bf5ba12065b746f3c1827c7a26056f888
loudringphone <loudringphone@gmail.com>
2024-02-04 22:49:00 +1100

    only flush those redis keys with user_count_ prefix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,5 +3,8 @@ require 'redis'
 # Create a Redis client instance
 REDIS = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379/0')
 
-# Clear the Redis db everytime the server is started
-REDIS.flushdb
+keys_to_delete = REDIS.keys("user_count_*")
+keys_to_delete.each do |key|
+  REDIS.del(key)
+end
+
