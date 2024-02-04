5b0af26a54b21088ab9fa75349b90dce9a7c0837
loudringphone <loudringphone@gmail.com>
2024-02-05 08:52:31 +1100

    seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,31 @@
+Image.destroy_all
+
+# Create images
+images = [
+  { title: 'Image 1', attachment: File.open(File.join(Rails.root, 'app', 'assets', 'images', 'image1.jpg')) },
+  { title: 'Image 2', attachment: File.open(File.join(Rails.root, 'app', 'assets', 'images', 'image2.jpg')) },
+  { title: 'Image 3', attachment: File.open(File.join(Rails.root, 'app', 'assets', 'images', 'image3.jpg')) },
+  { title: 'Image 4', attachment: File.open(File.join(Rails.root, 'app', 'assets', 'images', 'image4.jpg')) },
+  { title: 'Image 5', attachment: File.open(File.join(Rails.root, 'app', 'assets', 'images', 'image5.jpg')) }
+]
+
+# Seed images
+images.each do |image_params|
+  image = Image.new(image_params)
+  image.attachment = image_params[:attachment] # Assign attachment
+  image.save!
+end
+
+puts "#{ Image.count } seed images have been created"
+
+
+
+
+
+
+
+
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
