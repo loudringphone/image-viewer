4565ec6eb2a945a5407d0239954c6bb183108771
loudringphone <loudringphone@gmail.com>
2024-02-07 12:46:58 +1100

    could not get turbo to update value without cache

--- app/javascript/controllers/visitor_controller.js
+++ app/javascript/controllers/visitor_controller.js
@@ -11,44 +11,45 @@ export default class extends Controller {
   };
 
   connect() {
-    const turboCountElement = this.turboCountTarget;
-    document.addEventListener('turbo:load', function() {
-      const mutationCallback = (mutationList, observer) => {
-        for (const mutation of mutationList) {
-          if (mutation.type === "childList") {
-            const addedNodes = mutation.addedNodes;
+    // const turboCountElement = this.turboCountTarget;
+    // document.addEventListener('turbo:load', function() {
+    //   const turboNavigator = window.Turbo.navigator
+    //     if (turboNavigator.currentVisit) {
+    //       const location = turboNavigator.currentVisit.location.pathname;
+    //       const referrer = turboNavigator.currentVisit.referrer.pathname;
+    //       console.log(location, referrer);
+    //       if (location !== referrer) {
+    //       const mutationCallback = (mutationList, observer) => {
+    //         for (const mutation of mutationList) {
+    //           if (mutation.type === "childList") {
+    //             const addedNodes = mutation.addedNodes;
 
-            if (addedNodes.length > 0) {
-              const lastAddedNode = mutation.addedNodes[mutation.addedNodes.length - 1];
-              if (lastAddedNode instanceof Element && lastAddedNode.id.startsWith('image_')) {
-                clearTimeout(turboVisitTimeout);
-                console.log('The last added node has the element ID:', lastAddedNode.id);
-              }
-            }
-          }
-        }
-      };
+    //             if (addedNodes.length > 0) {
+    //               const lastAddedNode = mutation.addedNodes[mutation.addedNodes.length - 1];
+    //               if (lastAddedNode instanceof Element && lastAddedNode.id.startsWith('image_')) {
+    //                 clearTimeout(turboVisitTimeout);
+    //                 console.log('The last added node has the element ID:', lastAddedNode.id);
+    //               }
+    //             }
+    //           }
+    //         }
+    //       };
 
-      const observer = new MutationObserver(mutationCallback);
-      const turboVisit = () => {
-        observer.disconnect();
-        const turboNavigator = window.Turbo.navigator
-        if (turboNavigator.currentVisit) {
-          const location = turboNavigator.currentVisit.location.pathname;
-          const referrer = turboNavigator.currentVisit.referrer.pathname;
-          console.log(location, referrer);
-          if (location !== referrer) {
-            Turbo.visit(window.location.href);
-          }
-        } else {
-          Turbo.visit(window.location.href);
-        }
-      };
-      const turboVisitTimeout = setTimeout(turboVisit, 150);
-      observer.observe(turboCountElement, { childList: true });
-    }, { once: true});
+    //       const observer = new MutationObserver(mutationCallback);
+    //       const turboVisit = () => {
+    //         observer.disconnect();
+    //         return Turbo.visit(window.location.href);
+    //       };
+    //       const turboVisitTimeout = setTimeout(turboVisit, 150);
+    //       observer.observe(turboCountElement, { childList: true });
+    //     }
+    //   } else {
+    //     console.log('first visit')
+    //     return Turbo.visit(window.location.href);
+    //   }
+    // }, { once: true});
 
-
+    
     const navElement = this.navTarget;
     const countElement = this.countTarget;
     setTimeout(() => {
@@ -62,10 +63,10 @@ export default class extends Controller {
       { channel: "VisitorChannel", id: imageId },
       {
         connected: () => {
-          // console.log(`Connected to VisitorChannel ${imageId}`);
+          console.log(`Connected to VisitorChannel ${imageId}`);
         },
         disconnected: () => {
-          // console.log(`Bye VisitorChannel ${imageId}`);
+          console.log(`Bye VisitorChannel ${imageId}`);
           consumer.subscriptions.remove(this.subscription)
           window.removeEventListener('turbo:before-cache', this.handleBeforeCache)
         },
@@ -86,21 +87,21 @@ export default class extends Controller {
             if (data.img_id) {
               // console.log(data.img_id)
               const anchor = document.createElement('a');
-            anchor.id = 'next';
-            anchor.classList.add('cursor-pointer', 'ml-auto');
-            anchor.href = `/images/${data.img_id}`;
-            const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-            svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-            svg.setAttribute('viewBox', '0 0 24 24');
-            const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-            path.setAttribute('d', 'm21.707 11.293-7-7A1 1 0 0 0 13 5v3H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h10v3a1 1 0 0 0 1.707.707l7-7a1 1 0 0 0 0-1.414z');
-            path.style.fill = '#ccc';
-            svg.appendChild(path);
-            anchor.appendChild(svg);
-            const span = document.createElement('span');
-            span.textContent = 'Next';
-            anchor.appendChild(span);
-            navElement.appendChild(anchor);
+              anchor.id = 'next';
+              anchor.classList.add('cursor-pointer', 'ml-auto');
+              anchor.href = `/images/${data.img_id}`;
+              const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+              svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+              svg.setAttribute('viewBox', '0 0 24 24');
+              const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+              path.setAttribute('d', 'm21.707 11.293-7-7A1 1 0 0 0 13 5v3H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h10v3a1 1 0 0 0 1.707.707l7-7a1 1 0 0 0 0-1.414z');
+              path.style.fill = '#ccc';
+              svg.appendChild(path);
+              anchor.appendChild(svg);
+              const span = document.createElement('span');
+              span.textContent = 'Next';
+              anchor.appendChild(span);
+              navElement.appendChild(anchor);
             }
           }
           if (data.code == 'previous') {
